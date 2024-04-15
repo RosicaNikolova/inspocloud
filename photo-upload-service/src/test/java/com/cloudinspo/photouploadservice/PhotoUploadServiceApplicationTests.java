f2d7e4814dcef63ae5da7f66978a10a378de4679
RosicaNikolova <rosicanikolova1402@gmail.com>
2024-04-15 15:49:16 +0200

    testing pipeline build and test stages

--- photo-upload-service/src/test/java/com/cloudinspo/photouploadservice/PhotoUploadServiceApplicationTests.java
+++ photo-upload-service/src/test/java/com/cloudinspo/photouploadservice/PhotoUploadServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PhotoUploadServiceApplicationTests {
+class PhotoUpload  ServiceApplicationTests {
 
 //	@Test
 //	void contextLoads() {
