f2d53f2ebf0aa1738bf6e9495f679dc4b7be2565
RosicaNikolova <rosicanikolova1402@gmail.com>
2024-04-15 15:41:31 +0200

    test method commented

--- photo-management-service/src/test/java/com/cloudinspo/photomanagementservice/PhotoManagementServiceApplicationTests.java
+++ photo-management-service/src/test/java/com/cloudinspo/photomanagementservice/PhotoManagementServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PhotoManagementServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
