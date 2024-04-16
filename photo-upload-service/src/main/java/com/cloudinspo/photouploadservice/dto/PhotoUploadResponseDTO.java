939426059b1bbfad1c9167c951ca227526d124a5
RosicaNikolova <rosicanikolova1402@gmail.com>
2024-04-17 00:26:31 +0200

    api gateway implemented

--- photo-upload-service/src/main/java/com/cloudinspo/photouploadservice/dto/PhotoUploadResponseDTO.java
+++ photo-upload-service/src/main/java/com/cloudinspo/photouploadservice/dto/PhotoUploadResponseDTO.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Builder
-public class PhotoUploadResponseDTO {
+public class PhotoUploadResponseDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String uri;
     private String title;
     private List<String> tags;
