4627c42715c3259f6e1c667ce271aa30039de193
RosicaNikolova <rosicanikolova1402@gmail.com>
2024-05-27 10:58:51 +0200

    testing photo-management pipeline 3

--- photo-management-service/src/main/java/com/cloudinspo/photomanagementservice/controller/PhotoController.java
+++ photo-management-service/src/main/java/com/cloudinspo/photomanagementservice/controller/PhotoController.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class PhotoController {
     private final PhotoService photoService;
 
-    //Testing pipeline 3
+    //Testing pipeline 4
 
     @GetMapping("/photos")
     public List<Photo> getAllPhotos() {
